36bba83d48b3dfe67d1aba9fdf918396a93185e8
farhaan-sh <shf.cse@gmail.com>
2023-10-27 23:27:36 +0600

    transformers_pipelines update

--- .env/hfpipelines.py
+++ .env/hfpipelines.py
@@ -1,11 +1,16 @@
 from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 import torch
 import torch.nn.functional as F
 
 model_name = "distilbert-base-uncased-finetuned-sst-2-english"
 
-classifier = pipeline("sentiment-analysis", model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+
+classifier = pipeline("sentiment-analysis", model = model_name)
 results = classifier(["We are happy to show the transformers Library.",
                     "We hope you don't hate it."])
 
