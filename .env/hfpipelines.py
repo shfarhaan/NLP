72f59591b0f699bff5a92edab62409b1311c5da0
farhaan-sh <shf.cse@gmail.com>
2023-10-30 23:43:11 +0600

    torch.no_grad

--- .env/hfpipelines.py
+++ .env/hfpipelines.py
@@ -26,3 +26,32 @@ input_ids = tokenizer("We are happy to show the transformers Library.")
 print(f"    Tokens: {tokens}")
 print(f"Token IDs: {token_ids}")
 print(f"Token IDs: {input_ids}")
+
+
+X_train  = ["We are happy to show the transformers Library.",
+                    "We hope you don't hate it."]
+
+batch =  tokenizer(X_train, padding = True, truncation = True, max_length = 512, return_tensors = "pt")
+
+with torch.no_grad():
+    # outputs = model(batch) # For TensorFlow we don't need to unpack batch data
+    outputs = model(**batch) # For pytorch we need to unpack batch data
+    predictions = F.softmax(outputs.logits, dim=1)
+    print(predictions)
+    labels = torch.argmax(predictions, dim=1)
+    print(labels)
+
+    labels = [model.config.id2label[label_id] for label_id in labels.tolist()]
+    print(labels)
+
+
+
+
+
+
+
+
+
+
+
+
